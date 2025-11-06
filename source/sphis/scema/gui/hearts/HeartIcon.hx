d829c61912e487e1f711fe7ec443925146e65496
Viwu <sinconsistencia@gmail.com>
2025-11-06 16:49:26 -0600

    heart icon

---
+++ source/sphis/scema/gui/hearts/HeartIcon.hx
@@ -0,0 +1,13 @@
+package sphis.scema.gui.hearts;
+
+import flixel.FlxSprite;
+
+class HeartIcon extends FlxSprite
+{
+	override public function new()
+	{
+		super();
+
+		loadGraphic('assets/images/gui/heart.png');
+	}
+}
