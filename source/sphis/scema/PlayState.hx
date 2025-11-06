4a2dfa7f70d5c656ba23f5e517c755579eb607cd
Viwu <sinconsistencia@gmail.com>
2025-11-06 17:08:24 -0600

    give me hearts

--- source/sphis/scema/PlayState.hx
+++ source/sphis/scema/PlayState.hx
@@ -1,16 +1,26 @@
 package sphis.scema;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import sphis.scema.gui.GuiConstants;
+import sphis.scema.gui.hearts.HeartsGroup;
 
 class PlayState extends FlxState
 {
+	public var hearts:HeartsGroup;
+
 	override public function create()
 	{
 		super.create();
+
+		hearts = new HeartsGroup(20);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (FlxG.keys.justReleased.R)
+			hearts.health = FlxG.random.int(GuiConstants.MIN_HEALTH, GuiConstants.MAX_HEALTH);
 	}
 }
