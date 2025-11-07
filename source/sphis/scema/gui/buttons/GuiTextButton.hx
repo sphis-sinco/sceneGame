dc6163817ede86768d559f2181ef6ba54fad98e5
Viwu <sinconsistencia@gmail.com>
2025-11-06 21:01:14 -0600

    shadow

--- source/sphis/scema/gui/buttons/GuiTextButton.hx
+++ source/sphis/scema/gui/buttons/GuiTextButton.hx
@@ -3,6 +3,7 @@ package sphis.scema.gui.buttons;
 import flixel.FlxBasic;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
 import sphis.scema.gui.buttons.GuiButton.GuiButtonParameters;
 
 typedef GuiTextButtonParameters =
@@ -15,18 +16,29 @@ typedef GuiTextButtonParameters =
 class GuiTextButton extends FlxTypedGroup<FlxBasic>
 {
 	public var button:GuiButton;
+
 	public var text_field:FlxText;
+	public var text_field_shadow:FlxText;
 
 	override public function new(params:GuiTextButtonParameters)
 	{
 		super();
 
 		button = new GuiButton(params);
+
 		text_field = GuiText.drawText(params?.text_content ?? "N/A");
 		text_field.fieldWidth = button.snappedWidth;
 		text_field.alignment = CENTER;
 
+		text_field_shadow = GuiText.drawText(text_field.text);
+		text_field_shadow.fieldWidth = text_field.fieldWidth;
+		text_field_shadow.alignment = text_field.alignment;
+
+		text_field_shadow.color = FlxColor.BLACK;
+		text_field_shadow.alpha = 0.9;
+
 		add(button);
+		add(text_field_shadow);
 		add(text_field);
 	}
 
@@ -35,5 +47,6 @@ class GuiTextButton extends FlxTypedGroup<FlxBasic>
 		super.update(elapsed);
 
 		text_field.setPosition(button.x, button.y + ((button.height - text_field.height) / 2));
+		text_field_shadow.setPosition(text_field.x + 2, text_field.y + 2);
 	}
 }
