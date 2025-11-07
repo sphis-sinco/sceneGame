5fbf6dc6583ddc0159bba6435e430b778ccb72d1
Viwu <sinconsistencia@gmail.com>
2025-11-06 20:40:31 -0600

    GuiTextButton is a sprite group now

--- source/sphis/scema/gui/buttons/GuiTextButton.hx
+++ source/sphis/scema/gui/buttons/GuiTextButton.hx
@@ -1,5 +1,7 @@
 package sphis.scema.gui.buttons;
 
+import flixel.FlxBasic;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import sphis.scema.gui.buttons.GuiButton.GuiButtonParameters;
 
@@ -7,17 +9,22 @@ typedef GuiTextButtonParameters =
 {
 	> GuiButtonParameters,
 
-	var text:String;
+	var text_content:String;
 }
 
-class GuiTextButton extends GuiButton
+class GuiTextButton extends FlxTypedGroup<FlxBasic>
 {
-	public var text:FlxText;
+	public var button:GuiButton;
+	public var text_field:FlxText;
 
 	override public function new(params:GuiTextButtonParameters)
 	{
-		super(params);
+		super();
 
-		text = new FlxText(0, 0, 0,);
+		button = new GuiButton(params);
+		text_field = GuiText.drawText(params.text_content);
+
+		add(button);
+		add(text_field);
 	}
 }
