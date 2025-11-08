6b1c6162b6a40cb5ccd945dde6bd70f55f3c91e7
Viwu <sinconsistencia@gmail.com>
2025-11-07 23:41:20 -0600

    FONT_BITEND_DEMO define

--- source/sphis/scema/gui/text/GuiText.hx
+++ source/sphis/scema/gui/text/GuiText.hx
@@ -1,14 +1,26 @@
 package sphis.scema.gui.text;
 
 import flixel.math.FlxPoint;
+import flixel.system.FlxAssets;
 import flixel.text.FlxText;
 
 class GuiText
 {
+	public static var font(get, never):String;
+
+	static function get_font():String
+	{
+		#if FONT_BITEND_DEMO
+		return 'assets/fonts/Bitend DEMO.ttf';
+		#end
+
+		return FlxAssets.FONT_DEFAULT;
+	}
+
 	public static function drawText(text:String, ?position:FlxPoint)
 	{
 		var text = new FlxText(position?.x ?? 2, position?.y ?? 2, 0, text, 16);
-		text.font = 'assets/fonts/Bitend DEMO.ttf';
+		text.font = font;
 
 		return text;
 	}
