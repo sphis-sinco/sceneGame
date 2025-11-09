10fcb25ea52ec24e5b59e5f432be38d2c47aef89
Viwu <sinconsistencia@gmail.com>
2025-11-09 08:52:33 -0600

    Compilation time trace

--- source/sphis/scema/compiling/Postbuild.hx
+++ source/sphis/scema/compiling/Postbuild.hx
@@ -1,9 +1,39 @@
 package source.sphis.scema.compiling;
 
+import haxe.Timer;
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
+
 class Postbuild
 {
+	static var time_path:String = ".dev/time.txt";
+
 	static function main()
 	{
 		trace("Performing postbuild tasks...");
+
+		#if sys
+		var current_time:Float;
+		current_time = Timer.stamp();
+		if (FileSystem.exists(time_path))
+		{
+			trace("Found time path!");
+
+			var previous_time:Float = Std.parseFloat(File.getContent(time_path));
+
+			var difference:Float;
+			difference = current_time - previous_time;
+			difference = roundToTwoDecimals(difference);
+
+			trace("Compilation time: " + difference + "s");
+		}
+		#end
+	}
+
+	static function roundToTwoDecimals(value:Float):Float
+	{
+		return Math.round(value * 100) / 100;
 	}
 }
--- source/sphis/scema/compiling/Prebuild.hx
+++ source/sphis/scema/compiling/Prebuild.hx
@@ -1,9 +1,21 @@
 package source.sphis.scema.compiling;
 
+import haxe.Timer;
+#if sys
+import sys.io.File;
+#end
+
 class Prebuild
 {
+	static var time_path:String = ".dev/time.txt";
+
 	static function main()
 	{
 		trace("Performing prebuild tasks...");
+
+		#if sys
+		trace("Saving time file...");
+		File.saveContent(time_path, Std.string(Timer.stamp()));
+		#end
 	}
 }
