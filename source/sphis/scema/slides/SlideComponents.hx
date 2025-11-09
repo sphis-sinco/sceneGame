bec30cc7b3ce34f233eef6859388ed5c1f0a5978
Viwu <sinconsistencia@gmail.com>
2025-11-09 08:58:06 -0600

    fixed null components crash

--- source/sphis/scema/slides/SlideComponents.hx
+++ source/sphis/scema/slides/SlideComponents.hx
@@ -4,6 +4,9 @@ class SlideComponents
 {
 	public static function getComponent(slide:SlideData, name:String):Dynamic
 	{
+		if (slide.components == null)
+			return null;
+
 		for (component in slide.components)
 		{
 			if (component.name == name)
